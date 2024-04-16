27a9f5887c6e4199c234f94f39bf8eb1c47c9bb6
Jimmy Bourassa <jimmy.bourassa@shopify.com>
2024-04-16 11:29:36 -0400

    Add msgpack support

--- src/lib.rs
+++ src/lib.rs
@@ -138,6 +138,21 @@ where
                 Ok(Self(body))
             }
 
+            #[cfg(feature = "msgpack")]
+            b"application/msgpack" => {
+                let body = Bytes::from_request(req, state).await.map_err(|e| {
+                    tracing::error!(error = %e, "failed to ready request body as bytes");
+                    e.into_response()
+                })?;
+
+                let body = rmp_serde::from_slice(&body).map_err(|e| {
+                    tracing::error!(error = %e, "failed to deserialize request body as msgpack");
+                    MALFORMED_RESPONSE.into_response()
+                })?;
+
+                Ok(Self(body))
+            }
+
             _ => {
                 tracing::error!("unsupported accept header: {:?}", accept);
                 return Err((
@@ -211,6 +226,8 @@ impl AcceptExt for axum::http::HeaderMap {
             b"application/json" => Some("application/json"),
             #[cfg(feature = "cbor")]
             b"application/cbor" => Some("application/cbor"),
+            #[cfg(feature = "msgpack")]
+            b"application/msgpack" => Some("application/msgpack"),
             b"*/*" => Some(DEFAULT_CONTENT_TYPE_VALUE),
             _ => None,
         }
@@ -300,6 +317,27 @@ where
                     }
                     body.into_inner()
                 }
+                #[cfg(feature = "msgpack")]
+                "application/msgpack" => {
+                    let mut body = Vec::new();
+                    {
+                        let mut serializer =
+                            rmp_serde::Serializer::new(&mut body).with_struct_map();
+                        let mut serializer = <dyn erased_serde::Serializer>::erase(&mut serializer);
+                        if let Err(e) = payload.erased_serialize(&mut serializer) {
+                            tracing::error!(error = %e, "failed to deserialize request body as msgpack");
+
+                            let response: Response = (
+                                StatusCode::INTERNAL_SERVER_ERROR,
+                                "Failed to serialize response",
+                            )
+                                .into_response();
+                            return Ok(response);
+                        }
+                    }
+                    body
+                }
+
                 _ => vec![],
             };
 
@@ -916,6 +954,129 @@ mod test {
                     .encode(&mut writer)
                     .unwrap();
                     writer.into_inner()
+                });
+            }
+        }
+    }
+
+    #[cfg(feature = "msgpack")]
+    mod msgpack {
+        use std::collections::HashMap;
+
+        use super::*;
+
+        mod input {
+
+            use super::*;
+
+            #[tokio::test]
+            async fn test_can_read_input_with_specified_header() {
+                #[axum::debug_handler]
+                async fn handler(Negotiate(input): Negotiate<Example>) -> impl IntoResponse {
+                    format!("Hello, {}!", input.message)
+                }
+
+                let app = Router::new().route("/", post(handler));
+                let body = {
+                    let body = HashMap::from([("message", "test")]);
+                    rmp_serde::encode::to_vec_named(&body).unwrap()
+                };
+
+                let response = app
+                    .oneshot(
+                        Request::builder()
+                            .uri("/")
+                            .header(CONTENT_TYPE, "application/msgpack")
+                            .method("POST")
+                            .body(Body::from(body))
+                            .unwrap(),
+                    )
+                    .await
+                    .unwrap();
+
+                assert_eq!(response.status(), 200);
+                assert_eq!(
+                    response.into_body().collect().await.unwrap().to_bytes(),
+                    "Hello, test!"
+                );
+            }
+        }
+
+        mod output {
+            use super::*;
+
+            #[tokio::test]
+            async fn test_encode_as_requested() {
+                #[axum::debug_handler]
+                async fn handler() -> impl IntoResponse {
+                    Negotiate(Example {
+                        message: "Hello, test!".to_string(),
+                    })
+                }
+
+                let app = Router::new()
+                    .route("/", post(handler))
+                    .layer(NegotiateLayer);
+
+                let response = app
+                    .oneshot(
+                        Request::builder()
+                            .uri("/")
+                            .method("POST")
+                            .header(ACCEPT, "application/msgpack")
+                            .body(Body::empty())
+                            .unwrap(),
+                    )
+                    .await
+                    .unwrap();
+
+                assert_eq!(response.status(), 200);
+                assert_eq!(
+                    response.headers().get(CONTENT_TYPE).unwrap(),
+                    "application/msgpack"
+                );
+                assert_eq!(response.into_body().collect().await.unwrap().to_bytes(), {
+                    let body = HashMap::from([("message", "Hello, test!")]);
+                    rmp_serde::encode::to_vec_named(&body).unwrap()
+                });
+            }
+
+            #[tokio::test]
+            async fn test_retain_status_code() {
+                #[axum::debug_handler]
+                async fn handler() -> impl IntoResponse {
+                    (
+                        StatusCode::CREATED,
+                        Negotiate(Example {
+                            message: "Hello, test!".to_string(),
+                        }),
+                    )
+                }
+
+                let app = Router::new()
+                    .route("/", post(handler))
+                    .layer(NegotiateLayer);
+
+                let response = app
+                    .oneshot(
+                        Request::builder()
+                            .uri("/")
+                            .method("POST")
+                            .header(ACCEPT, "application/msgpack")
+                            .body(Body::empty())
+                            .unwrap(),
+                    )
+                    .await
+                    .unwrap();
+
+                assert_eq!(response.status(), StatusCode::CREATED);
+                assert_eq!(
+                    response.headers().get(CONTENT_TYPE).unwrap(),
+                    "application/msgpack"
+                );
+                assert_eq!(response.into_body().collect().await.unwrap().to_bytes(), {
+                    let body = HashMap::from([("message", "Hello, test!")]);
+                    rmp_serde::encode::to_vec_named(&body).unwrap()
                 });
             }
         }
